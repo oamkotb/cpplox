c2d71081ef8d7d35ebf1e77bee75c3e742185017
seif9116 <smetwall@ualberta.ca>
2024-06-20 21:53:26 -0600

    Worked on GenerateAstCpp.py some more - used a different version of the c++ port to update - about 95% done with this version - if a different c++ port is given shouldnt take long to update - also made it so defineAst is expecting within the field all the information of the constructor as in const * & ,etc

--- tool/GenerateAstCpp.py
+++ tool/GenerateAstCpp.py
@@ -4,11 +4,39 @@ from typing import List, TextIO
 
 def defineVisitor(
         header_file: TextIO,
-        source_file: TextIO,
         base_name: str,
         types: List[str]) -> None:
     # TODO: Write implementation of visitor creation
-    ...
+    header_file.write("    struct Visitor {\n")
+    for type_info in types:
+        type_name: str = type_info.split(':')[0].strip()
+        header_file.write(f"        virtual R visit{type_name}Expr(const class {type_name}& expr)=0;\n")
+    header_file.write("    };\n")
+
+
+def defineType(
+        header_file: TextIO,
+        source_file: TextIO,
+        base_name: str,
+        class_name: str,
+        fields: str) -> None:
+
+    # Beginning
+    header_file.write(f"    class {class_name} : public {base_name}<R> {{\n")
+    header_file.write("    public:\n")
+
+    # Constructor
+    header_file.write(f"        {class_name}({fields});\n")
+
+    # Accept
+    header_file.write(f"    R accept(const typename {base_name}<R>::Visitor& visitor) const override;\n")
+
+    # Variables
+    fields_list: List[str] = fields.split(',')
+    for field in fields_list:
+        header_file.write(f"{field.strip()};\n")
+
+    header_file.write("};\n")
 
 
 def defineAst(
@@ -22,13 +50,32 @@ def defineAst(
 
     with open(header_path, 'w') as header_file:
         with open(source_path, 'w') as source_file:
-            source_file.write("#praga once\n")
-            source_file.write("\n")
-            source_file.write(f"#include {base_name}.h\n")
-            source_file.write("template <class R>\n")
+            # Creating beginning of header file
+            header_file.write("#praga once\n")
+            header_file.write("\n")
+            header_file.write('#include "Token.h"')
+            header_file.write("\n")
+            header_file.write("template <class R>\n")
+            header_file.write(f"class {base_name}{{\n")
+            header_file.write("public:\n")
 
-            source_file.write()
-            defineVisitor(header_file, source_file, base_name, types)
+            # Creating beginning of source file
+            source_file.write(f"#include {base_name}.h\n")
+            source_file.write("\n")
+
+            defineVisitor(header_file, base_name, types)
+
+            # The base accept() method
+            header_file.write("    virtual R accept(const Visitor& visitor) const = 0;\n")
+
+            # The AST classes.
+            for type_info in types:
+                splitted_types: List[str] = type_info.split(':')
+                class_name: str = splitted_types[0].strip()
+                fields: str = splitted_types[1].strip()
+                defineType(header_file, source_file, base_name, class_name, fields)
+
+            header_file.write("};\n")
 
 
 def main():

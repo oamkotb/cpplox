74b257ce4245f47ffe325f1a9d9593eebce89f18
seif9116 <smetwall@ualberta.ca>
2024-06-27 21:32:40 -0600

    95% done GenerateAstCpp.py - just need to test the code but it should correctly generate the Expr.h file

--- tool/GenerateAstCpp.py
+++ tool/GenerateAstCpp.py
@@ -1,42 +1,106 @@
 import os
-from typing import List, TextIO
+from typing import List, TextIO, Dict
 
 
-def defineVisitor(
+def parseTypes(
+        types: List[List[str]]) -> List[Dict[str, str]]:
+    '''Parses the types'''
+
+    splitted_types: List[List[str]] = [type_info.split(":")
+                                       for type_info in types]
+    parsed_types: List[Dict[str, str]] = [{'class_name': type_info[0].strip(),
+                                           'fields': type_info[1].strip()}
+                                          for type_info in splitted_types]
+
+    return parsed_types
+
+
+def defineVisitorHeader(
         header_file: TextIO,
         base_name: str,
         types: List[str]) -> None:
-    # TODO: Write implementation of visitor creation
-    header_file.write("    struct Visitor {\n")
+    header_file.write("  struct Visitor\n")
+    header_file.write("  {\n")
+
     for type_info in types:
-        type_name: str = type_info.split(':')[0].strip()
-        header_file.write(f"        virtual R visit{type_name}Expr(const class {type_name}& expr)=0;\n")
-    header_file.write("    };\n")
+        header_file.write(f"    virtual R visit{type_info['class_name']}Expr"
+                          f"(const {type_info['class_name']} expr) = 0;\n")
 
 
-def defineType(
+def defineTypeHeader(
         header_file: TextIO,
-        source_file: TextIO,
         base_name: str,
         class_name: str,
         fields: str) -> None:
-
     # Beginning
-    header_file.write(f"    class {class_name} : public {base_name}<R> {{\n")
-    header_file.write("    public:\n")
+    header_file.write(f"  class{class_name} : public {base_name}<R>\n")
+    header_file.write("{\n")
+    header_file.write("public:\n")
 
     # Constructor
-    header_file.write(f"        {class_name}({fields});\n")
+    header_file.write(f"    {class_name}({fields})")
 
-    # Accept
-    header_file.write(f"    R accept(const typename {base_name}<R>::Visitor& visitor) const override;\n")
+    # Accept method
+    header_file.write("    R accept(const Visitor& visitor) override\n")
+    header_file.write("{\n")
+    header_file.write(f"      return visitor.visitr{class_name}Expr(this);\n")
+    header_file.wrtie("\n")
 
-    # Variables
-    fields_list: List[str] = fields.split(',')
-    for field in fields_list:
-        header_file.write(f"{field.strip()};\n")
+    # declarations
+    fields: List[str] = fields.split(",")
+    fields: List[str] = [field_info.strip() for field_info in fields]
+
+    for type_decl in fields:
+        header_file.write(f"    {type_decl};\n")
 
     header_file.write("};\n")
+
+
+def defineHeader(
+        header_output_dir: str,
+        base_name: str,
+        types: List[List[str]]) -> None:
+
+    header_path: str = os.path.join(header_output_dir, f"{base_name}.h")
+    with open(header_path, 'w') as header_file:
+        # Creating beginning of header file
+        header_file.write("#praga once\n")
+        header_file.write("\n")
+        header_file.write('#include "Token.h"')
+        header_file.write("\n")
+        header_file.write("template <class R>\n")
+        header_file.write(f"class {base_name}{{\n")
+        header_file.write("public:\n")
+
+        # Predefinitons for types
+        for type_info in types:
+            header_file.write(f"  clase {type_info[1]};\n")
+
+        defineVisitorHeader(header_file, base_name, types)
+
+        # The base accept method
+        header_file.write("    R accept(const Visitor& visitor) override")
+
+        # The AST Methods
+        for type_info in types:
+            defineTypeHeader(header_file,
+                             base_name,
+                             type_info['class_name'],
+                             type_info['fields'])
+
+        header_file.write("};\n")
+
+
+def defineSource(
+        source_output_dir: str,
+        base_name: str,
+        types: List[List[str]]) -> None:
+
+    source_path: str = os.path.join(source_output_dir, f"{base_name}.cc")
+    with open(source_path, 'w') as source_file:
+        # Creating beginning of source file
+        source_file.write(f"#include {base_name}.h\n")
+        source_file.write("\n")
 
 
 def defineAst(
@@ -45,37 +109,10 @@ def defineAst(
         base_name: str,
         types: List[str]) -> None:
 
-    header_path: str = os.path.join(header_output_dir, f"{base_name}.h")
-    source_path: str = os.path.join(source_output_dir, f"{base_name}.cc")
+    parsed_types = parseTypes(types)
 
-    with open(header_path, 'w') as header_file:
-        with open(source_path, 'w') as source_file:
-            # Creating beginning of header file
-            header_file.write("#praga once\n")
-            header_file.write("\n")
-            header_file.write('#include "Token.h"')
-            header_file.write("\n")
-            header_file.write("template <class R>\n")
-            header_file.write(f"class {base_name}{{\n")
-            header_file.write("public:\n")
-
-            # Creating beginning of source file
-            source_file.write(f"#include {base_name}.h\n")
-            source_file.write("\n")
-
-            defineVisitor(header_file, base_name, types)
-
-            # The base accept() method
-            header_file.write("    virtual R accept(const Visitor& visitor) const = 0;\n")
-
-            # The AST classes.
-            for type_info in types:
-                splitted_types: List[str] = type_info.split(':')
-                class_name: str = splitted_types[0].strip()
-                fields: str = splitted_types[1].strip()
-                defineType(header_file, source_file, base_name, class_name, fields)
-
-            header_file.write("};\n")
+    defineHeader(header_output_dir, base_name, parsed_types)
+    defineSource(source_output_dir, base_name, parsed_types)
 
 
 def main():
@@ -83,10 +120,10 @@ def main():
     source_output_dir = input("Enter the source file directory: ")
 
     defineAst(header_output_dir, source_output_dir, "Expr", [
-            "Binary  : Expr left, Token operator, Expr right",
-            "Grouping: Expr expression",
-            "Literal : Object value",
-            "Unary   : Token operator, Expr right"
+            "Binary  : const Expr*& left, const Token& oper, const Expr*& right",
+            "Grouping: const Expr*& expression",
+            "Literal : const Token::LiteralValue& value",
+            "Unary   : const Token& operator, const Expr*& right"
     ])
 
 
